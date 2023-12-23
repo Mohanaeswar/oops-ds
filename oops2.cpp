6f472e8748acc06061307ad01d54dd8729a62dfe
Mohanaeswar <133188870+Mohanaeswar@users.noreply.github.com>
2023-12-23 11:16:11 +0530

    Update and rename c++ 2.cpp to oops2.cpp

--- oops2.cpp
+++ oops2.cpp
@@ -1,14 +1,16 @@
-#include<iostream>
-using namespace std;
+#include<iostream.h>
+#include<conio.h>
 int area(int);
 int area(int , int);
 float area(float);
 int main()
 {
+	clrscr();
 	cout<<"\nThe area of square: "<<area(8);
 	cout<<"\nThe area of recatngle: "<<area(5,10);
 	cout<<"\nThe area of circle: "<<area(5.56f);
 	return 0;
+	getch();
 }
 int area(int side)
 {
